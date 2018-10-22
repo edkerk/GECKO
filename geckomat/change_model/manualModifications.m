b859397ffd63d4c5bbeaa4966975ae6aab0381d9
Eduard Kerkhoven <eduardk@chalmers.se>
2018-10-22 16:16:25 +0200

    feat: manualModifications correct kcat values
    - as identified from sensitivity analysis without proteomics data and with initial set of automatically assigned kcat values

--- geckomat/change_model/manualModifications.m
+++ geckomat/change_model/manualModifications.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % model = manualModifications(model)
-% 
+%
 % Benjamin J. Sanchez. Last edited: 2017-10-29
 % Ivan Domenzain.      Last edited: 2018-05-28
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -69,7 +69,7 @@ for i = 1:length(model.rxns)
                 if sum(strcmp(model.enzymes,uniprots{j}{k})) == 0
                     model = addProtein(model,uniprots{j}{k},kegg,swissprot);
                 end
-                newMets{k} = ['prot_' newMets{k}];     
+                newMets{k} = ['prot_' newMets{k}];
             end
             %Add new protein stoich. coeffs to rxn:
             kvalues = kcats(j)./stoich{j};
@@ -84,6 +84,15 @@ for i = 1:length(model.rxns)
     %Get the proteins that are part of the i-th rxn
     prot_pos = find(~cellfun(@isempty,strfind(model.mets,'prot_')));
     int_pos  = intersect(subs_pos,prot_pos)';
+    %%%%%%%%%%%%%%%%%%%%%%%%%%  Individual Changes:  %%%%%%%%%%%%%%%%%%%%%%%%
+    for j = 1:length(int_pos)
+        enzName = model.mets(int_pos(j));
+        %%%%%%%%%%%%%%%%%% MANUAL CURATION FOR TOP GROWTH LIMITING ENZYMES:
+        [newValue,modifications] = curation_growthLimiting(reaction,enzName,MW_set,modifications);
+        if ~isempty(newValue)
+            model.S(int_pos(j),i) = newValue;
+        end
+    end
     disp(['Improving model with curated data: Ready with rxn #' num2str(i)])
 end
 
@@ -93,7 +102,7 @@ rem_rxn = false(size(model.rxns));
 for i = 1:length(model.rxns)-1
     for j = i+1:length(model.rxns)
         if isequal(model.S(:,i),model.S(:,j)) && model.lb(i) == model.lb(j) && ...
-           model.ub(i) == model.ub(j) 
+                model.ub(i) == model.ub(j)
             rem_rxn(j) = true;
             disp(['Removing repeated rxn: ' model.rxns{i} ' & ' model.rxns{j}])
         end
@@ -149,18 +158,75 @@ model = removeIncorrectPathways(model);
 
 % Map the index of the modified Kcat values to the new model (after rxns removals)
 modifications = mapModifiedRxns(modifications,model);
-
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function modified = mapModifiedRxns(modifications,model)
-    modified = [];
-    for i=1:length(modifications{1})
-        rxnIndex = find(strcmp(model.rxnNames,modifications{2}(i)),1);
-        str      = {horzcat(modifications{1}{i},'_',num2str(rxnIndex))};
-        modified = [modified; str];
-    end
+modified = [];
+for i=1:length(modifications{1})
+    rxnIndex = find(strcmp(model.rxnNames,modifications{2}(i)),1);
+    str      = {horzcat(modifications{1}{i},'_',num2str(rxnIndex))};
+    modified = [modified; str];
+end
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Modify the top growth limiting enzymes that were detected by the
+% modifyKcats.m script in a preliminary run.
+function [newValue,modifications] = curation_growthLimiting(reaction,enzName,MW_set,modifications)
+newValue = [];
+reaction = string(reaction);
+% chorismate synthase (Q9KXQ4/EC4.2.3.5) - model specified substrate 
+% 5-O-(1-Carboxyvinyl)-3-phosphoshikimate is synonymous to the BRENDA
+% specified substrate 5-enolpyruvylshikimate 3-phosphate. Changed manually
+% to match kcat of wild-type N. crassa enyzme (doi: 10.1111/
+% j.1742-4658.2008.06305.x)
+if strcmpi('prot_Q9KXQ4',enzName)
+    if contains(reaction,'chorismate synthase (')
+        newValue         = -(0.87*3600)^-1; % BRENDA: WT N. crassa
+        modifications{1} = [modifications{1}; 'Q9KXQ4'];
+        modifications{2} = [modifications{2}; reaction];
+    end
+    
+% phosphoribosylformylglycinamidine synthase (Q9RKK5/EC6.3.5.3) - kcat
+% automatically suggested was for ammonium as substrate, not glutamine.
+% Instead, use specific activity as provided in BRENDA, which was asayed
+% with glutamine according to original paper (doi:10.1021/bi00432a017)
+elseif strcmpi('prot_Q9RKK5',enzName)
+    if contains(reaction,'phosphoribosylformylglycinamidine synthase (')
+        newValue         = -(2.15*60*MW_set)^-1; % BRENDA: WT E. coli
+        modifications{1} = [modifications{1}; 'Q9RKK5'];
+        modifications{2} = [modifications{2}; reaction];
+    end
+% methylmalonate-semialdehyde dehydrogenase (malonic semialdehyde)
+% (Q9L1J1/EC6.3.5.3) - kcat automatically assigned was from archaea,
+% instead use value from Bacillus subtilis (doi:10.1074/jbc.M110.213280).
+elseif strcmpi('prot_Q9L1J1',enzName)
+    if contains(reaction,'methylmalonate-semialdehyde dehydrogenase (malonic semialdehyde) (')
+        newValue         = -(2.2*60*MW_set)^-1; % BRENDA: WT B. subtilis
+        modifications{1} = [modifications{1}; 'Q9L1J1'];
+        modifications{2} = [modifications{2}; reaction];
+    end
+% phosphoribosyl-ATP pyrophosphatase (Q9EWK0/EC3.6.1.31) - kcat
+% automatically assigned was calculated from specific activity in
+% Salmonella enterica, but the reported value was measured in cell extract,
+% not from purified enzyme. Instead, use specific activity from
+% S. cerevisiae (PMID:379004).
+elseif strcmpi('prot_Q9EWK0',enzName)
+    if contains(reaction,'phosphoribosyl-ATP pyrophosphatase (')
+        newValue         = -(332*60*MW_set)^-1; % BRENDA: WT E. coli
+        modifications{1} = [modifications{1}; 'Q9EWK0'];
+        modifications{2} = [modifications{2}; reaction];
+    end
+% % glyceraldehyde-3-phosphate dehydrogenase (Q9Z518/EC1.2.1.12) - kcat
+% elseif strcmpi('prot_Q9Z518',enzName)
+%     if contains(reaction,'glyceraldehyde-3-phosphate dehydrogenase (')
+%         newValue         = -(332*60*MW_set)^-1; % BRENDA: WT B. subtilis
+%         modifications{1} = [modifications{1}; 'Q9Z518'];
+%         modifications{2} = [modifications{2}; reaction];
+    end
+end
+
